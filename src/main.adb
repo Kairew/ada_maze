8862b254599e40195d432ca4806508b2cf02d229
Grnd0s <groundzeroes@protonmail.com>
2022-01-27 20:53:20 +0100

    add gyro demo

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,41 @@
-with Game;
+with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
+--  The "last chance handler" is the user-defined routine that is called when
+--  an exception is propagated. We need it in the executable, therefore it
+--  must be somewhere in the closure of the context clauses.
+
+with STM32.Board;           use STM32.Board;
+with HAL.Bitmap;            use HAL.Bitmap;
+pragma Warnings (Off, "referenced");
+with BMP_Fonts;
+with LCD_Std_Out;
+
+with gyro_demo;
 
 procedure Main is
+
+   BG : Bitmap_Color := (Alpha => 255, others => 0);
 begin
-    Game.Play;
+
+   --  Initialize LCD
+   Display.Initialize;
+   Display.Initialize_Layer (1, ARGB_8888);
+
+   LCD_Std_Out.Set_Font (BMP_Fonts.Font12x12);
+   LCD_Std_Out.Current_Background_Color := BG;
+
+   --  Clear LCD (set background)
+   Display.Hidden_Buffer (1).Set_Source (BG);
+   Display.Hidden_Buffer (1).Fill;
+
+   LCD_Std_Out.Clear_Screen;
+   Display.Update_Layer (1, Copy_Back => True);
+   LCD_Std_Out.Clear_Screen;
+
+
+   gyro_demo.Gyro_test;
+
+   loop
+         Display.Update_Layer (1, Copy_Back => True);
+   end loop;
+
 end Main;
